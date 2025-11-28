862211cd3e76cdde980f512b1d0b38c67946809e
Ashish Mishra <ash1174@gmail.com>
2025-11-27 19:43:12 -0500

    Remove Python serverless function files (backend deployed on Render)

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# Add the project root to sys.path so we can import 'backend'
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from backend.app.main import app
